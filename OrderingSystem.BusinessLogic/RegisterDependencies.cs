ed46d650eaa87b6df4254e6a76daebbfc77c2f2b
m.elsabbagh <m.elsabbagh@MUHMD-SABAGH-PC>
2026-04-03 04:16:37 +0200

    dependencies registered for each layer

---
+++ OrderingSystem.BusinessLogic/RegisterDependencies.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OrderingSystem.BusinessLogic.MapperConfigration;
+using OrderingSystem.BusinessLogic.Services;
+using OrderingSystem.Global.Interfaces.Services;
+
+namespace OrderingSystem.BusinessLogic
+{
+    public static class RegisterDependencies
+    {
+        public static IServiceCollection AddBusinessLogicDependencies(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(MappingConfig));
+
+            services.AddScoped<ICustomerService, CustomerService>();
+            services.AddScoped<IOrderService, OrderService>();
+
+            return services;
+        }
+    }
+}
---
+++ OrderingSystem.DataAccess/RegisterDependencies.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderingSystem.DataAccess.Data;
+using OrderingSystem.Global.Interfaces.Repositories;
+using OrderingSystem.Global.Interfaces.UnitOfWork;
+
+namespace OrderingSystem.DataAccess
+{
+    public static class RegisterDependencies
+    {
+        public static IServiceCollection AddDataAccessDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseLazyLoadingProxies().UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(Repositories.GenericRepository<>));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork.UnitOfWork>();
+            return services;
+        }
+    }
+}
